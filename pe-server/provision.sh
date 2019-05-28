03a79124267c0b447b898efde533793ff7835974
orcutt989 <james.orcutt@puppet.com>
2019-05-28 16:43:38 -0500

    Release choice logic

--- pe-server/provision.sh
+++ pe-server/provision.sh
@@ -10,14 +10,23 @@ osascript -e "tell application \"Tunnelblick\"" -e "connect \"puppet_corporate_v
 # Get internal release numbers as an array
 PE_RELEASES=($(curl -s http://enterprise.delivery.puppetlabs.net/archives/releases/ | grep -o '[0-9]\{4\}.[0-9].[[0-9]' | sort -r  --unique))
 
-# Ask user what version of PE they want or get from a variable or default to latest
-LATEST={PE_RELEASES[0]}
+# Latest version
+LATEST=${PE_RELEASES[0]}
 
+# Ask user what version they want
 while true; do
-    echo
-    read -p "What version of Puppet Enterprise would you like?: " -i LATEST -e answer
-    echo "you answered: $answer"
-    [[ -z ${PE_RELEASES[0]} ]] && echo "Release choice does not exist"
+    NUM=1
+    for RELEASE in "${PE_RELEASES[@]}"
+    do
+        echo -e "[$NUM]: $RELEASE" 
+        ((NUM++))
+    done
+    echo -n "What version of Puppet Enterprise would you like? [$LATEST]: "
+    read CHOICE
+    CHOICE=${CHOICE:-1}
+    INDEX=CHOICE-1
+    echo "You chose: [$CHOICE]: ${PE_RELEASES[$INDEX]}"
+    [[ -z ${PE_RELEASES[$CHOICE]} ]] && echo "Release does not exist, try again."
     break
 done
 
