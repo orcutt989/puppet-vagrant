17aeecb611ab074499af515b1e7b537006a0bd35
orcutt989 <james.orcutt@puppet.com>
2019-05-28 16:12:19 -0500

    added array for releases

--- pe-server/provision.sh
+++ pe-server/provision.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
 
 # Connect to VPN
-if osascript -e "tell application \"Tunnelblick\"" -e "connect \"puppet_corporate_vpn\"" -e "end tell";
-then echo "worked!";
-fi
+osascript -e "tell application \"Tunnelblick\"" -e "connect \"puppet_corporate_vpn\"" -e "end tell" || $echo \
+"VPN Connection failed, check to see you have bot the Tunnelblick VPN Client and the puppet_corporate_vpn profile installed"
+
 
 # If no release number variable then...
 
-# Get internal release numbers
-curl -s http://enterprise.delivery.puppetlabs.net/archives/releases/ | grep -o '[0-9]\{4\}.[0-9].[[0-9]' | sort -r  --unique
+# Get internal release numbers as an array
+PE_RELEASES=($(curl -s http://enterprise.delivery.puppetlabs.net/archives/releases/ | grep -o '[0-9]\{4\}.[0-9].[[0-9]' | sort -r  --unique))
 
 # Ask user what version of PE they want or get from a variable or default to latest
+LATEST={PE_RELEASES[0]}
+
+while true; do
+    echo
+    read -p "What version of Puppet Enterprise would you like?: " -i LATEST -e answer
+    echo "you answered: $answer"
+    [[ -z ${PE_RELEASES[0]} ]] && echo "Release choice does not exist"
+    break
+done
 
 # Based on variable or text input curl to release
 # i.e. http://enterprise.delivery.puppetlabs.net/archives/releases/2019.1.0/puppet-enterprise-2019.1.0-el-7-x86_64.tar
-
 
